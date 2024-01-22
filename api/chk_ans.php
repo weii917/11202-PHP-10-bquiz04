ce35962e6b7b5ec2fabb0e32c528151fdc2cfda5
wei <lin951016@gmail.com>
2024-01-22 16:35:29 +0800

    註解

--- api/chk_ans.php
+++ api/chk_ans.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+// 當get到的值與session裡已經存放的值做比對，成功傳1 否則0
 echo ($_SESSION['ans']==$_GET['ans'])?1:0;
 
 ?>
--- api/chk_pw.php
+++ api/chk_pw.php
@@ -6,6 +6,7 @@ unset($_POST['table']);
 $db=new DB($table);
 $chk=$db->count($_POST);
 
+// 依傳過來的資料表可能是mem或是admin，將acc存進，以區分是會員還是管理著要分開存放session依據$table不同欄位名稱
 if($chk){
     echo $chk;
     $_SESSION[$table]=$_POST['acc'];

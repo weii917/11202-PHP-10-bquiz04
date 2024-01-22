3d8cad0e6c82f296cee36d7b04c4b8b67c9a09bf
wei <lin951016@gmail.com>
2024-01-22 14:45:50 +0800

    完成註冊會員檢查及註冊會員資料新增至資料庫

---
+++ api/reg.php
@@ -0,0 +1,9 @@
+<?php
+include_once 'db.php';
+
+$_POST['regdate']=date("Y-m-d");
+$Mem->save($_POST);
+
+
+
+?>
--- front/reg.php
+++ front/reg.php
@@ -25,23 +25,47 @@
     </tr>
     <tr>
         <td class="tt ct">電子信箱</td>
-        <td class="pp"><input type="text" name="email" id="eamil"></td>
+        <td class="pp"><input type="text" name="email" id="email"></td>
     </tr>
 </table>
 <div class="ct">
-    <button>註冊</button>
-    <button>重置</button>
+    <button onclick="reg()">註冊</button>
+    <button onclick="clean()">重置</button>
 </div>
 <!-- 檢測帳號的function ， 將acc的value送到api撈出資料庫計算筆數，echo的結果回到res，在此做判斷==1代表與資料庫有符合一筆帳號 -->
 <script>
+    function reg(){
+        let user={
+            name:$("#name").val(),
+            acc:$("#acc").val(),
+            pw:$("#pw").val(),
+            tel:$("#tel").val(),
+            addr:$("#addr").val(),
+            email:$("#email").val(),
+        }
+        $.get("./api/chk_acc.php",{acc:user.acc},(res)=>{
+            if(parseInt(res)==1 || user.acc=='admin'){
+                alert(`此帳號${user.acc}已被使用`)
+            }else{
+               $.post("./api/reg.php",user,()=>{
+                location.href('?do=login')
+               })
+            }
+        })
+    }
+    // 因為用ajax不知道e結果什麼時候回來，所以無法chkacc()直接取用來判斷，這裡直接複製一個在註冊前做判斷
     function chkacc() {
         let acc = $("#acc").val()
         $.get("./api/chk_acc.php",{acc},(res)=>{
-            if(parseInt(res)==1){
+            if(parseInt(res)==1 || acc=='admin'){
                 alert(`此帳號${acc}已被使用`)
             }else{
                 alert(`此帳號${acc}可以使用`)
             }
         })
+    }
+
+    function clean(){
+        $("#name,#acc,#pw,#tel,#addr,#email").val('');
     }
 </script>

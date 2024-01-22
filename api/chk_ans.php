b1e9bb798d5cf58296de36820e37d3d04ae5e7b7
wei <lin951016@gmail.com>
2024-01-22 15:49:44 +0800

    完成會員登入及驗證碼檢查功能

---
+++ api/chk_ans.php
@@ -0,0 +1,5 @@
+<?php
+session_start();
+($_SESSION['ans']==$_GET['ans'])?1:0;
+
+?>
--- front/login.php
+++ front/login.php
@@ -20,7 +20,27 @@
             $_SESSION['ans']=$a+$b;
             echo $a . " + " . $b . " = ";
             ?>
-            <input type="text" name="chk" id="chk"></td>
+            <input type="text" name="ans" id="ans"></td>
     </tr>
 </table>
-<div class="ct"><button>確認</button></div>
+<div class="ct">
+    <button onclick="login('mem')">確認</button>
+</div>
+<script>
+    function login(table){
+        $.get('./api/chk_ans.php',{ans:$("#ans").val()},(chk)=>{
+            if(parseInt(chk)==0){
+                alert("驗證碼錯誤，請重新輸入")
+            }else{
+                $.post("./api/chk_pw.php",{table,acc:$("#acc").val(),pw:$("#pw").val()},(res)=>{
+                    if(parseInt(res)==0){
+                        alert("帳號或密碼錯誤，請重新輸入")
+
+                    }else{
+                        location.href='index.php';
+                    }
+                })
+            }
+        })  
+    }
+</script>

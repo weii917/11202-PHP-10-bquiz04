89cbabdffe9b89c5fd05d63189d6885483e9cad2
wei <lin951016@gmail.com>
2024-01-31 16:05:51 +0800

    建立ajax取得分類選單函式

--- api/db.php
+++ api/db.php
@@ -146,5 +146,6 @@ $Bottom=new DB('bottom');
 $Mem=new DB('mem');
 $Admin=new DB('admin');
 $Type=new DB('type');
+$Goods=new DB('goods');
 
 ?>
--- back/add_goods.php
+++ back/add_goods.php
@@ -48,3 +48,25 @@
         <input type="button" value="返回">
     </div>
 </form>
+
+<script>
+getTypes('big',0)
+
+$("#big").on("change",function(){
+    getTypes('mid',$("#big").val())
+})
+
+function getTypes(type,big_id){
+    $.get("./api/get_types.php",{big_id},(types)=>{
+        switch(type){
+            case 'big':
+                $("#big").html(types)
+                getTypes('mid',$("#big").val())
+            break;
+            case 'mid':
+                $("#mid").html(types)
+            break;
+        }
+    })
+}
+</script>

d5442467ef00caebec31c9e8b6a05dd9e51ee580
wei <lin951016@gmail.com>
2024-01-31 09:40:50 +0800

    完成修改管理員權限

--- back/add_admin.php
+++ back/add_admin.php
@@ -8,7 +8,7 @@
         </tr>
         <tr>
             <td class="tt ct">密碼</td>
-            <td class="pp"><input type="text" name="pw" id="pw"></td>
+            <td class="pp"><input type="password" name="pw" id="pw"></td>
         </tr>
         <tr>
             <td class="tt ct">權限</td>

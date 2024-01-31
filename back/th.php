420bd52343e8a2526357dc7d7d043fddf83b703a
wei <lin951016@gmail.com>
2024-01-31 15:31:08 +0800

    完成新增商品的頁面

--- back/th.php
+++ back/th.php
@@ -83,7 +83,7 @@
 </script>
 
 <h2 class="ct">商品管理</h2>
-<div class="ct"><button>新增商品</button></div>
+<div class="ct"><button onclick="location.href='?do=add_goods'">新增商品</button></div>
 <table class="all">
     <tr class="tt ct">
         <td>編號</td>
